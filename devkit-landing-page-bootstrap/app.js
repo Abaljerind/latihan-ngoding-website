d0ef8e2d35df75e748b1dc93370fcfd4439e0af8
Abaljerind <abaljerinda@gmail.com>
2023-04-09 22:06:57 +0700

    add: content & style nya & text tambahan di js

---
+++ devkit-landing-page-bootstrap/app.js
@@ -0,0 +1,9 @@
+var paragraph1 = document.getElementById("devkit-text1");
+var text1 = document.createTextNode("Develop anything.");
+
+paragraph1.appendChild(text1);
+
+var paragraph2 = document.getElementById("devkit-text2");
+var text2 = document.createTextNode("components built from scratch.");
+
+paragraph2.appendChild(text2);
